51cb9358836e68f55a704e1d3e51e284bb7166a8
Saumyen <saumyen45@gmail.com>
2023-08-27 14:54:28 +0530

    first

---
+++ script.js
@@ -0,0 +1,7 @@
+import Typed from 'typed.js';
+
+var typed = new Typed('#element', {
+    strings: ['Web Developer','Django Developer','Graphics Designer','Footballer','Chess Player'],
+    typeSpeed: 40,
+    loop: true,
+    });
